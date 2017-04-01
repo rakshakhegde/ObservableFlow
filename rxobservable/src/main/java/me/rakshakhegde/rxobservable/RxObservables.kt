19e1917b57fde8db382928a0b45c6eaa66327a65
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-04-01 23:11:21 +0530

    rxjava2 update

---
+++ rxobservable/src/main/java/me/rakshakhegde/rxobservable/RxObservables.kt
@@ -0,0 +1,44 @@
+package com.instavans.instavansshippers.utils.ObservablesKotlinUtils
+
+import android.databinding.ObservableField
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
+import me.rakshakhegde.observableflow.onPropertyChanged
+
+/**
+ * Created by rakshakhegde on 01/04/17.
+ */
+
+fun <T> rx(observable: ObservableField<T>): Observable<T> {
+
+	return Observable.create { emitter ->
+
+		val changedCallback = observable.onPropertyChanged { emitter.onNext(get()) }
+
+		emitter.setCancellable { observable.removeOnPropertyChangedCallback(changedCallback) }
+	}
+}
+
+@JvmOverloads
+fun <T> Observable<T>.toObservableField(defaultVal: T? = null,
+                                        compositeDisposable: CompositeDisposable? = null)
+		: ObservableField<T> {
+
+	val observableField = ObservableField<T>(defaultVal)
+	val disposable = subscribeWith(object : DisposableObserver<T>() {
+		override fun onNext(item: T) {
+			observableField.set(item)
+		}
+
+		override fun onComplete() {
+			// ignore
+		}
+
+		override fun onError(e: Throwable) {
+			e.printStackTrace()
+		}
+	})
+	compositeDisposable?.add(disposable)
+	return observableField
+}
