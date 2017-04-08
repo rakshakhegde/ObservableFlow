136e82945600daf2f59d07f2fb8f051738096e11
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-04-08 19:13:58 +0530

    toObservableField for a Single

--- rxdatabinding/src/main/java/me/rakshakhegde/rxdatabinding/RxObservables.kt
+++ rxdatabinding/src/main/java/me/rakshakhegde/rxdatabinding/RxObservables.kt
@@ -2,6 +2,7 @@ package me.rakshakhegde.rxdatabinding
 
 import android.databinding.ObservableField
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import me.rakshakhegde.observableflow.bind
 
@@ -26,13 +27,6 @@ fun <T> rx(observable: ObservableField<T>): Observable<T> {
 	}
 }
 
-/**
- * Convert a Rx Observable to Android Data Binding's ObservableField.
- * Pass in a CompositeDisposable if you want to manually dispose this off later. But for
- * a nicer API, you could use RxLifecycle by Trello.
- * @see <a href="https://github.com/trello/RxLifecycle/tree/2.x">
- * https://github.com/trello/RxLifecycle/tree/2.x</a>
- */
 @JvmOverloads
 fun <T> Observable<T>.toObservableField(defaultVal: T? = null,
                                         compositeDisposable: CompositeDisposable? = null
@@ -40,6 +34,17 @@ fun <T> Observable<T>.toObservableField(defaultVal: T? = null,
 
 	val observableField = ObservableField<T>(defaultVal)
 	val disposable = subscribe(observableField::set)
+	compositeDisposable?.add(disposable)
+	return observableField
+}
+
+@JvmOverloads
+fun <T> Single<T>.toObservableField(defaultVal: T? = null,
+                                    compositeDisposable: CompositeDisposable? = null
+): ObservableField<T> {
+
+	val observableField = ObservableField<T>(defaultVal)
+	val disposable = subscribe { item -> observableField.set(item) }
 	compositeDisposable?.add(disposable)
 	return observableField
 }
