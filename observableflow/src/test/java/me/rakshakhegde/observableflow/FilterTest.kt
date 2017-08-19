cfb398733946ae37a21b20e979dead811e9f5f33
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-08-19 16:10:52 +0530

    filter test

---
+++ observableflow/src/test/java/me/rakshakhegde/observableflow/FilterTest.kt
@@ -0,0 +1,56 @@
+package me.rakshakhegde.observableflow
+
+import android.databinding.ObservableField
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Created by Rakshak.R.Hegde on 19-Aug-17.
+ */
+class FilterTest {
+
+	@Test
+	fun verify_filters_immediately_if_predicate_true() {
+		val src = ObservableField("even")
+
+		val dst = src.filter { it.length % 2 == 0 }
+
+		assertEquals(expected = "even", actual = dst.get())
+
+		src.set("odd")
+		assertEquals(expected = "even", actual = dst.get())
+	}
+
+	@Test
+	fun verify_doesnt_filter_immediately_if_predicate_false() {
+		val src = ObservableField("odd")
+
+		val dst = src.filter { it.length % 2 == 0 }
+
+		assertEquals(expected = null, actual = dst.get())
+
+		src.set("even")
+		assertEquals(expected = "even", actual = dst.get())
+	}
+
+	@Test
+	fun verify_filter_default_val_assigned_if_predicate_false() {
+		val src = ObservableField("odd")
+
+		val dst = src.filter(defaultVal = "default") { it.length % 2 == 0 }
+
+		assertEquals(expected = "default", actual = dst.get())
+
+		src.set("even")
+		assertEquals(expected = "even", actual = dst.get())
+	}
+
+	@Test
+	fun verify_filter_default_val_changed_if_predicate_true() {
+		val src = ObservableField("even")
+
+		val dst = src.filter(defaultVal = "default") { it.length % 2 == 0 }
+
+		assertEquals(expected = "even", actual = dst.get())
+	}
+}
